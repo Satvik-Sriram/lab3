9e26b4b7f3a96b9d0c059201b4e8b9d4422bb8d8
SatvikS7 <79161050+SatvikS7@users.noreply.github.com>
2023-01-26 15:20:25 -0800

    Update ListTest.java

--- ListTest.java
+++ ListTest.java
@@ -3,9 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.*;
 
+interface StringChecker { boolean checkString(String s); }
+
+
 public class ListTest {
     @Test
     public void testFilter() {
         List<String> input1 = new ArrayList<>();
+        input1.add("");
+        assertArrayEquals(new String[]{""}, ListExamples.filter(input1, ""));
     }
 }
