64947aa5619dfa84a6913b9d7f21853623a142e0
SatvikS7 <79161050+SatvikS7@users.noreply.github.com>
2023-01-26 15:16:14 -0800

    new file

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -28,12 +28,19 @@ public class ArrayExamples {
   static double averageWithoutLowest(double[] arr) {
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
+    boolean removed = false;
     for(double num: arr) {
       if(num < lowest) { lowest = num; }
     }
     double sum = 0;
     for(double num: arr) {
-      if(num != lowest) { sum += num; }
+      if(num != lowest) { 
+        sum += num; 
+      } else if(!removed) {
+        removed = true;
+      } else {
+        sum += num;
+      }
     }
     return sum / (arr.length - 1);
   }
---
+++ ListTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.*;
+
+public class ListTest {
+    @Test
+    public void testFilter() {
+        List<String> input1 = new ArrayList<>();
+    }
+}
